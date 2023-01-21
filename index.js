a9f5098ee3b38c1c2d6090dc3eb4440061d560ba
Kevin Duggan <kevinduggan95@gmail.com>
2023-01-20 18:03:51 -0600

    looplab

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards(name) {
+    let frank = []
+    for (let x = 0; x < name.length; x++) {
+        frank[x] = `Thank you, ${name[x]}, for the wonderful surprise gift!`
+    }
+    return frank
+
+}
+function countDown(number) {
+    for ( let x = number ;x >= 0; x--) {
+        console.log(x);
+    }
+    
+}
